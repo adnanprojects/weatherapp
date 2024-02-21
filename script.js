720aa65046cd659033f29c98277e310a1f73f6be
adnanprojects <adnanchaudhary99@gmail.com>
2024-02-21 23:23:34 +0530

    update changes

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ const search = document.getElementById('search-button');
 const cityName = document.getElementById('city-name');
 const cityTime = document.getElementById('city-time');
 const cityTemp = document.getElementById('city-temp');
+const button = document.getElementById('getlocation');
 
+// Render Result data
+function render(result) {
+    cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
+    cityTime.innerText = result.location.localtime;
+    cityTemp.innerText = `${result.current.temp_c} C`;
+}
+
+// Using city name
 async function getData(city) {
     const data = await fetch(`http://api.weatherapi.com/v1/current.json?key=2cded80fbe954916a77100421242002&q=${city}&aqi=yes`);
     return data.json();
@@ -14,8 +23,25 @@ async function getData(city) {
 search.addEventListener('click', async () => {
     const city = input.value;
     const result = await getData(city);
-    cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
-    cityTime.innerText = result.location.localtime;
-    cityTemp.innerText = `${result.current.temp_c} C`;
+    render(result);
+    input.value = '';
+});
 
+// Using Location
+async function getGeoData(latitude, longitude) {
+    const data = await fetch(`http://api.weatherapi.com/v1/current.json?key=2cded80fbe954916a77100421242002&q=${latitude},${longitude}&aqi=yes`);
+    return await data.json();
+}
+
+async function getLocation(position) {
+    const result = await getGeoData(position.coords.latitude, position.coords.longitude);
+    render(result);
+}
+
+function failedToGet() {
+    console.log('failed to get the location');
+}
+
+button.addEventListener('click', async () => {
+    const location = navigator.geolocation.getCurrentPosition(getLocation, failedToGet);
 });
